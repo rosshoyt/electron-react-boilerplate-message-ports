23f2fd7cda85f52705f06bb74258f050318af0eb
Ross Hoyt <43669429+rosshoyt@users.noreply.github.com>
2022-01-11 13:00:05 -0800

    Added file selection, file reading utility functions

--- src/main/util.ts
+++ src/main/util.ts
@@ -2,6 +2,9 @@
 import { URL } from 'url';
 import path from 'path';
 
+const fs = require('fs');
+const { dialog } = require('electron');
+
 export let resolveHtmlPath: (htmlFileName: string) => string;
 
 if (process.env.NODE_ENV === 'development') {
@@ -15,4 +18,35 @@ if (process.env.NODE_ENV === 'development') {
   resolveHtmlPath = (htmlFileName: string) => {
     return `file://${path.resolve(__dirname, '../renderer/', htmlFileName)}`;
   };
+}
+
+export function getBufferFromFile(fullPath: string): string {
+  const fileData = fs.readFileSync(
+    path.normalize(fullPath),
+    // 'utf8',
+    (err: any, data: any) => {
+      if (err) throw err;
+      console.log('read file at', fullPath);
+      return data;
+    }
+  );
+  if (fileData !== undefined) {
+    return fileData;
+  }
+  return 'error';
+}
+
+export async function openFolderSelector(): Promise<string[]> {
+  console.log('Opening folder dialog');
+  let filePaths: string[] = [];
+  try {
+    const result = await dialog.showOpenDialog({
+      properties: ['openFile'],
+    });
+    filePaths = result.filePaths;
+  } catch (error) {
+    console.log(error);
+  }
+  console.log('Returning paths', filePaths);
+  return filePaths;
 }
